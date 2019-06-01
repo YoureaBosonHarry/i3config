659fbc0027995f8b2dc08aabb93c6ffcbf595df5
YoureaBosonHarry <rath3.14159@protonmail.com>
2019-06-01 00:36:30 +0000

    Removed shell script from this repo

--- gitcli.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# This simple program is meant to be run with i3wm and git cli
-# The bindsym command is found in my i3, but can be modded as bindsym $mod+key /path/to/file.sh
-#token_file = /home/gentooben/.config/i3/gittoken.txt
-cat /home/gentooben/.config/i3/gittoken.txt | xclip -sel clip
